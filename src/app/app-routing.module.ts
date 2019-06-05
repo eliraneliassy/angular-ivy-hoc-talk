a5da96003eba61b99726c6d70efafe2007a64301
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-06-05 20:35:45 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'feature',
+    loadChildren: './feature/feature.module#FeatureModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
